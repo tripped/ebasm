91419e94261aa249b9804a0a13618adb57a8a5f4
Trip Volpe <mraccident@gmail.com>
2011-04-16 16:56:48 -0700

    Substantially rewrote disasm.py for more semantic opcode tracking. Still need to fix status register tracking.

--- disasm.py
+++ disasm.py
@@ -1,16 +1,15 @@
-#
-# A quick-and-dirty disassembler
+##
+# A quick-and-dirty 65816 disassembler.
 #
 
 from array import array
 import re
 
 
-
-
-#
+#------------------------------------------------------------------------------
 # The definition of the 65816 instruction set.
-#
+#------------------------------------------------------------------------------
+
 instruction_set = {
     # ADC
     0x69 : 'ADC #const*', # '*' = actual width of const determined by m bit
@@ -353,150 +352,262 @@ instruction_set = {
     0xFB : 'XCE',
 }
 
-
-
 assert(len(instruction_set) == 256)
 
 
 
-class Status(object):
-    """Status holds the current context while disassembling. It includes the
-       values of the M and X status register bits, and the address of the
-       instruction currently being handled."""
-    def __init__(self):
-        self.op = 0     # Current opcode
-        self.m = 0      # State of m bit of flags register
-        self.x = 0      # State of x bit of flags register
-        self.pc = 0     # Value of the program counter register
-        self.adr = 0    # Next read address
+#------------------------------------------------------------------------------
+# Stuff for reading and formatting operands
+#------------------------------------------------------------------------------
+
 
 #
-# Some functions for reading primitives from a stream, possibly modified by
-# current status register
+# Functions for reading primitives from a stream. Each returns a tuple
+# containing the primitive value and its size in bytes. 
 #
-def byte(src, status):
-    status.adr += 1
-    return next(src)
+def byte(src):
+    return next(src), 1
 
-def short(src, status):
-    status.adr += 2
-    return next(src) | (next(src) << 8)
+def short(src):
+    return (next(src) | (next(src) << 8)), 2
 
-def long(src, status):
-    status.adr += 3
-    return next(src) | (next(src) << 8) | (next(src) << 16)
+def long(src):
+    return (next(src) | (next(src) << 8) | (next(src) << 16)), 3
 
+
+#
+# We need these to treat bytes and shorts as signed integers, e.g. when
+# computing absolute targets of relative jumps.
+#
 def signedbyte(n):
-    """Sign-extends a byte to a signed integer."""
+    '''Sign-extends a byte to a signed integer.'''
     return n - 2 * (n & 0x80)
 
 def signedshort(n):
-    """Sign-extends a short to a signed integer."""
+    '''Sign-extends a short to a signed integer.'''
     return n - 2 * (n & 0x8000)
 
-#
-# The magical operands format map!
-#
-# Each type of operand is processed by a pair of functions: a consumer and a
-# stringifier. The consumer reads the correct number of operand bytes from a
-# stream, and the stringifier renders the operand in a desired fashion.
-# 
-# Each function also takes a 'status' parameter which tracks the state of the
-# disassembly. It's implied that the consumer mutates the status.
-#
-# In a nutshell:
-#   consume: stream -> status -> operand-value
-#   stringify: operand-value -> status -> string
-#   formats: operand-type -> (consume, stringify)
-#
-formats = {
-    'const*': (
-        lambda s,p: byte(s,p) if p.m else short(s,p),
-        lambda n,p: '${:0{w}X}'.format(n, w = 2 if p.m else 4)
-    ),
-    'const+': (
-        lambda s,p: byte(s,p) if p.x else short(s,p),
-        lambda n,p: '${:0{w}X}'.format(n, w = 2 if p.m else 4)
-    ),
-    'short': (short,        lambda n,p: '${:04X}'.format(n)),
-    'byte' : (byte,         lambda n,p: '${:02X}'.format(n)),
-    'addr' : (short,        lambda n,p: '${:04X}'.format(n)),
 
-    # PC-relative operands are transformed into absolute addresses
-    'near' : (byte,         lambda n,p: '${:04X}'.format(p.pc + signedbyte(n))),
-    'nearx': (short,        lambda n,p: '${:04X}'.format(p.pc + 1 + signedshort(n))),
-    'long' : (long,         lambda n,p: '${:06X}'.format(n)),
-    'dp'   : (byte,         lambda n,p: '${:02X}'.format(n)),
-    'sr'   : (byte,         lambda n,p: '{}'.format(n)),
+#
+# Each type of 65816 operand is processed by a pair of functions: a consumer
+# and a stringifier. The consumer reads the correct number of bytes from a
+# stream, and the stringifier renders the operand as a string.
+#
+# Each function takes a status as its second parameter, to handle those operand
+# formats that depend on the current status of the CPU.
+#
+formats = { 'const*', 'const+', 'short', 'byte', 'addr', 'near', 'nearx',
+            'long', 'dp', 'sr' }
+
+operand_consumers = {
+    'const*': lambda s,p: byte(s) if p.m else short(s),
+    'const+': lambda s,p: byte(s) if p.x else short(s),
+    'short':  lambda s,p: short(s),
+    'byte':   lambda s,p: byte(s),
+    'addr':   lambda s,p: short(s),
+    'near':   lambda s,p: byte(s),
+    'nearx':  lambda s,p: short(s),
+    'long':   lambda s,p: long(s),
+    'dp':     lambda s,p: byte(s),
+    'sr':     lambda s,p: byte(s)
 }
 
+operand_stringifiers = {
+    'const*': lambda n,p: '${:0{w}X}'.format(n, w = 2 if p.m else 4),
+    'const+': lambda n,p: '${:0{w}X}'.format(n, w = 2 if p.x else 4),
+    'short':  '${:04X}'.format,
+    'byte':   '${:02X}'.format,
+    'addr':   '${:04X}'.format,
+    'near':   lambda n,p: '${:04X}'.format(p.pc + 2 + signedbyte(n)),
+    'nearx':  lambda n,p: '${:04X}'.format(p.pc + 3 + signedshort(n)),
+    'long':   '${:06X}'.format,
+    'dp':     '${:02X}'.format,
+    'sr':     '{}'.format
+}
 
+#
 # Build a regexp for finding operand types in a string. We sort the keys in
 # order of descending length to avoid missing a longer key by finding a short
 # key first, e.g., "BRL nearx" matches "near" before it matches "nearx".
-
+#
 pattern = re.compile(
-        '|'.join(map(re.escape, sorted(formats.keys(), key=len, reverse=True)))
+        '|'.join(map(re.escape, sorted(formats, key=len, reverse=True)))
     )
 
 
-def makeparser(description):
-    """Constructs an operand parsing/disassembling function for the described
-       instruction. The returned function, when given a byte stream and status
-       object, will read a correctly-sized operand from the stream and return
-       a very pretty disassembly of the instruction.
-       
-       Example:
-            makeparser('ADC #const*')
-            
-       makeparser looks at the operand type ('const*') and employs appropriate
-       consume and stringify functions to handle the operand. The mnemonic (in
-       this case ADC) is for printing only and has no effect on the behavior of
-       the parser.
-       
-       The status object passed to the parse function should be initialized
-       with the appropriate settings of the P and PC registers."""
+def makereader(description):
+    '''Makes a reader function for the specified opcode, e.g.,
 
-    consume = lambda s,p: None
-    stringify = lambda n,p: ''
+            f = makereader('LDX #const+')
 
-    # Which operand format does this instruction use?
+       The returned function can be applied to a byte stream and a status
+       object to pull a correctly-sized operand from the stream; for example,
+       f(src, Status(x=1,...)) will read only one byte from src, because the
+       LDX #const+ instruction matches the size of the CPU's X register.'''
+
     match = pattern.search(description)
     if match:
-        consume, stringify = formats[match.group(0)]
+        return operand_consumers[match.group(0)]
+    else:
+        return lambda *args: None
 
-    # Replace the operand format with a placeholder for stringify
+
+def makestringifier(description):
+    '''Makes a stringifier function for the specified opcode, e.g.,
+
+            f = makestringifier('LDX #const+')
+    
+       The returned function can be applied to an operand value and a status
+       object to get an assembly language representation of the instruction,
+       e.g.,  f(32, Status(x=1,...))  will return "LDX #$20" '''
+
+    match = pattern.search(description)
+
+    if not match:
+        return lambda *args: description
+
+    stringifier = operand_stringifiers[match.group(0)]
     description = pattern.sub('{}', description)
 
-    # Define the actual parsing function and return it as a closure
-    def parser(src, status):
-        operand = consume(src, status)
-        return description.format(stringify(operand, status))
+    return lambda n,s: description.format(stringifier(n, s))
 
-    return parser
 
+#
+# Make a reader and stringifier for every opcode in the 65816 instruction set
+#
+readers = { k : makereader(v) for k,v in instruction_set.items() }
+stringifiers = { k : makestringifier(v) for k,v in instruction_set.items() }
+
+
+
+
+#------------------------------------------------------------------------------
+# 
+# 
+#
+#------------------------------------------------------------------------------
+
+
+
+from collections import namedtuple
+
+#
+# Status represents the state of the 65816 at a particular point in time. It
+# consists of the values of the program counter register and the m and x bits
+# from the CPU status register.
+#
+Status = namedtuple('Status', ['pc', 'm', 'x'])
+
+
+class Instruction(object):
+    '''An instruction is an instance of a 65816 machine instruction, including
+       the machine opcode, its operand, and the status of the CPU at that point
+       in the program.'''
+
+    def __init__(self, op, operand, status):
+        self.op = op
+        if operand:
+            self.operand = operand[0]
+            self.operand_len = operand[1]
+        else:
+            self.operand = 0
+            self.operand_len = 0
+        self.status = status
+
+    def __len__(self):
+        return 1 + self.operand_len
+
+    def __iter__(self):
+        '''Iterates over the raw bytes of the instruction.'''
+        yield self.op
+        for i in range(self.operand_len):
+            yield (self.operand >> i*8) & 0xFF
+
+    def __str__(self):
+        '''Returns the instruction as an assembly language string.'''
+        return stringifiers[self.op](self.operand, self.status)
+
+
+
+
+def disassembly(inst, address=True, machine=True):
+    '''Returns a string representation of the disassembly of an instruction.
+        @param inst The instruction to disassemble
+        @param address If true, the instruction's address will be included
+        @param machine If true, the raw machine code will be included'''
+
+    extras = '' 
+
+    if address:
+        extras += '{bank:X}/{offset:X}: '.format(
+            bank = (inst.status.pc & 0xFF0000) >> 16,
+            offset = inst.status.pc & 0xFFFF)
+    if machine:
+        extras += '{:13}'.format(' '.join(map('{:02X}'.format, inst)))
+
+    return extras + str(inst)
 
 #
 # This is horrible, but getting slightly better
 #
 
+def instruction(src, status):
+    '''Reads a single instruction from a stream and returns a pair (inst, stat)
+       where inst is an object representing the instruction, and stat is the
+       'successor' status of the instruction, i.e. the state of the CPU after
+       executing the instruction.
+
+        @param src An iterable byte stream from which to read the instruction
+        @param status An object recording state of the 65816 at the place in
+                 the program where the instruction occurs; must include m, x
+                 flags and pc register value.'''
+
+    # Get the opcode and its operand
+    op = next(src)
+    operand = readers[op](src, status)
+
+    # Now we have enough information to compute the successor status. It'll go
+    # like this:
+    #  - First, the program counter of the successor is incremented
+    #     - successor.pc = status.pc + 1 + len(operand)
+    #  - Then we compute the new status of m and x flags:
+    #     - If the op is SEP or REP, the status change is computed from
+    #       its operand
+    #     - If the op is a JSL/JSR, we can do one of two things:
+    #        1. Assume that it resets m and x to 0. This is probably a good
+    #           assumption in most cases.
+    #        2. Examine the function being jumped to, if necessary reading
+    #           and disassembling it first, to see if it changes m or x.
+    #           Determining this in general is of course impossible, but we
+    #           could probably make some good guesses.
+    #     - In all other cases, m and x are unchanged.
+    #        
+
+    inst = Instruction(op, operand, status)
+    newstatus = Status(pc = status.pc + len(inst), m = status.m, x = status.x)
+
+    return (inst, newstatus)
+
+
+
 def disassemble(src, base):
-    parsers = dict([(i, makeparser(x)) for i,x in instruction_set.items()])
 
     # Initialize status
-    status = Status()
-    status.adr = base + 0xC00000
-    status.pc = base & 0xFFFF
+    status = Status(base, 0, 0)
 
-    while status.op != 0x6B and status.op != 0x60:
-        # Store the bank and PC offset for the current instruction before reading it
-        bank   = (status.adr & 0xFF0000) >> 16
-        offset = (status.adr & 0xFFFF)
+    instructions = []
 
-        # Read next instruction
-        status.op = byte(src, status)
-        status.pc = (status.adr + 1) & 0xFFFF
-        print('{:02X}/{:04X} {}'.format(bank, offset, parsers[status.op](src, status)))
+    inst = Instruction(0,0,0)
+
+    while inst.op != 0x6B and inst.op != 0x60:
+        inst,status = instruction(src, status)
+        instructions += inst
+        print(disassembly(inst))
+
+
+    print('Read {} instructions!'.format(len(instructions)))
+        
+        
 
 
 def loadfile(filename):
@@ -513,12 +624,19 @@ def iterfrom(container, offset):
             raise StopIteration()
 
 
-
-
 if __name__ == '__main__':
 
-    address = 0xb4df #0x1A9D
+    import sys
+    if len(sys.argv) < 3:
+        print('''Usage:
+    disasm.py <romfile> <hexoffset>''')
+        exit(1)
 
-    src = iterfrom(loadfile('earthbound.smc'), address)
+    filename = sys.argv[1]
+    address = int(sys.argv[2], 16)
+
+    src = iterfrom(loadfile(filename), address)
 
     disassemble(src, address)
+
+
